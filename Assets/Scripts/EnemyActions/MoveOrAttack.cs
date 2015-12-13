25cf0df43e7ee91382ec9c2d9e335b9a8bc48105
Itai <d00m.poet@gmail.com>
2015-12-13 20:09:35 +0200

    ant enemy

---
+++ Assets/Scripts/EnemyActions/MoveOrAttack.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveOrAttack : EnemyAction {
+	public int damage;
+	public int maxMove;
+	public int minMove;
+	public int minRange;
+	public int maxRange;
+	public override void ExecuteAction(Enemy enemy, Player player)
+	{
+		base.ExecuteAction(enemy, player);
+		int distanceToPlayer = Mathf.Abs(player.boardPosition - enemy.boardPosition);
+		if (distanceToPlayer >=minRange && distanceToPlayer <= maxRange)
+		{
+			player.TakeDamage(1);
+		}
+		else
+		{
+			enemy.CheckMove(enemy.facing*Random.Range(minMove,maxMove+1));
+		}
+	}
+}
