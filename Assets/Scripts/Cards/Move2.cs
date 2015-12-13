0a959eb52f21746417e2330425bc5c848dfccbcb
Itai <d00m.poet@gmail.com>
2015-12-13 20:10:02 +0200

    move 2 changed

--- Assets/Scripts/Cards/Move2.cs
+++ Assets/Scripts/Cards/Move2.cs
@@ -6,13 +6,28 @@ public class Move2 : Card {
 	protected override void ExecuteStage(Player player, int stage)
 	{
 		base.ExecuteStage(player, stage);
+		int distanceToEnemy = Mathf.Abs(player.boardPosition - player.target.boardPosition);
 		if (GameManager.instance.level == 0)
 		{
-			player.CheckMove(2);
+			if (player.boardPosition < player.target.boardPosition && distanceToEnemy == 2)
+			{
+				player.CheckMove(2 * -player.facing);
+			}
+			else
+			{
+				player.CheckMove(2);
+			}
 		}
 		else
 		{
-			player.CheckMove(2 * player.facing);
+			if (distanceToEnemy == 2)
+			{
+				player.CheckMove(2 * -player.facing);
+			}
+			else
+			{
+				player.CheckMove(2 * player.facing);
+			}
 		}
 	}
 }
